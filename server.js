8929f85e1321ee9377f5beb05031b329e91d564e
Roger G Nascimento <roger.goulart.nascimento@gmail.com>
2023-01-23 22:34:37 -0300

    desafio-01

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get("/clientes", function (req, res) {
   res.status(200).send(clientes);
 });
 
+app.get("/clientes/:id", function (req, res) {
+  const { id } = req.params;
+  const cliente = clientes.filter((c) => c.id == id);
+
+  res.status(200).send(cliente);
+});
+
 app.listen(3005, () => {
   console.log("servidor rodando na porta 3005");
 });
